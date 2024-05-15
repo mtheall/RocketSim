f5eff70e6c92cecf42365d8918fc55466905c164
Michael Theall <pigman46@gmail.com>
2024-05-14 20:38:21 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ RocketSim = Extension(
 
 setup(
   name = "RocketSim",
-  version = "2.1.0.post1",
+  version = "2.1.0.post2",
   description = "This is Rocket League!",
   cmdclass = {"build_ext": build_ext_ex},
   ext_modules = [RocketSim],
